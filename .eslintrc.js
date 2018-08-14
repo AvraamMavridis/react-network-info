ca7a4f7a82f06e674efb71cf15296e5d7e9f5e89
AvraamMavridis <avraammauridis@gmail.com>
2018-08-14 19:10:41 +0200

    Chore: Tidy up the project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	extends: [ 'avraam', 'plugin:react/recommended' ],
+	globals: {
+		React: false,
+		ReactDOM: false
+	},
+	rules: {
+		'class-methods-use-this': 0,
+		'consistent-return': 0
+	}
+};
